c4db595792bdf2641d0ac6b2102bb2a5ae55f96e
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-09-11 01:09:35 -0300

    classificação retornando mensagem completa

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import numpy as np
+from model import embed
+from model import criar_embeddings_treino
 
 app = Flask(__name__)
 CORS(app)
+embeddings_treino = criar_embeddings_treino()
+
+
+def criar_mensagem_problema(tipo):
+    if tipo == 'pastilhas de freio':
+        return 'Aah, claro! Ao que tudo indica, o problema do seu veículo é o desgaste das pastilhas de freio.'
+    elif tipo == 'velas de ignição':
+        return 'Pelo que você disse, parece que o problema do seu veículo está nas velas de ignição.'
+    elif tipo == 'transmissão':
+        return 'De acordo com meu conhecimento, a causa do que está acontecendo é um problema na transmissão do veículo.'
+    elif tipo == 'não identificado':
+        return 'Desculpe, não entendi. Você poderia descrever sua situação mais detalhadamente?'
 
 
 # função que recebe entrada do usuário e classifica o problema
-def classificar_entrada(entrada_usuario):
-    return f"Resposta para: {entrada_usuario}"
+def classificar_entrada(texto_entrada):
+    global embeddings_treino
+
+    # computando embeddings da entrada do usuário
+    emb_entrada = embed(texto_entrada)
+
+    # criando lista com a similaridade da entrada com cada uma das frases de treino e suas classificações
+    resultado = []
+    for i in embeddings_treino:
+        similaridade = np.inner(emb_entrada, i[2])
+        resultado.append([i[0], similaridade])
+
+    # ordenando a lista resultado a partir da maior similaridade
+    classificacao = sorted(resultado, key=lambda x: x[1], reverse=True)
+
+    tipo = classificacao[0][0] if classificacao[0][1] > 0.35 else 'não identificado'
+    resposta = criar_mensagem_problema(tipo)
+
+    return resposta
 
 
 # Rota para receber dados do frontend
@@ -15,7 +47,7 @@ def classificar_entrada(entrada_usuario):
 def classificar():
     data = request.json
     entrada = data.get('input')
-    response = classificar_entrada(entrada_usuario=entrada)
+    response = classificar_entrada(texto_entrada=entrada)
     return jsonify({'response': response})
 
 
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import tensorflow_hub as hub
 import tensorflow_text
-import numpy as np
+from corpus import frases_pastilhas_freio
+from corpus import frases_velas_ignicao
+from corpus import frases_transmissao
 
 
 # carregando modelo
@@ -13,3 +15,20 @@ except Exception as e:
 # função que retorna os embeddings dos textos passados
 def embed(texto):
     return model(texto)
+
+
+def criar_embeddings_treino():
+    global frases_pastilhas_freio
+    global frases_velas_ignicao
+    global frases_transmissao
+    # criando lista de treino no formato:
+    # [('classificação do problema', frase, embedding da frase)]
+
+    embeddings_pastilhas = [('pastilhas de freio', frase, embed(frase)) for frase in frases_pastilhas_freio]
+
+    embeddings_velas = [('velas de ignição', frase, embed(frase)) for frase in frases_velas_ignicao]
+
+    embeddings_transmissao = [('transmissão', frase, embed(frase)) for frase in frases_transmissao]
+
+    embeddings_treino = embeddings_pastilhas + embeddings_velas + embeddings_transmissao
+    return embeddings_treino
